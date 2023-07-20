8d3e57082ba9a5409869eb7440920ce4266fb959
Can Alpay <can.alpay@gmail.com>
2023-07-20 17:24:17 +0300

    Initial Commit

---
+++ FruitApiCall/FruitApiCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitApiCallApp.swift
+//  FruitApiCall
+//
+//  Created by Can Alpay on 20.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct FruitApiCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
